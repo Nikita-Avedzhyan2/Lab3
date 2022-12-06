cc19b53050ebc7be20e77aa5eef332c5333e9505
Nikita-Avedzhyan2 <113437068+Nikita-Avedzhyan2@users.noreply.github.com>
2022-12-06 20:35:24 +0300

    Initial commit

---
+++ public class Main {.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+    int num = 10;
+    for(int i = 2; i < num; i++){
+        for(int j = 2; j < i; j++){
+            if(i % j == 0){
+                System.out.println(i);
+            }
+     }
+    }
+    System.out.println(Down);
+}
